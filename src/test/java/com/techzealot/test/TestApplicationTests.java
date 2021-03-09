7f99bbe08ca04c16178b0102eeb637d2e8a16c04
techzealot <techzealot@foxmail.com>
2021-03-09 16:43:14 +0800

    init

---
+++ src/test/java/com/techzealot/test/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techzealot.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
